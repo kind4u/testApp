d25903db7de5ff5ca7603bc1ac53d91e2a6b0383
kind4u <kind4u@naver.com>
2020-03-11 15:40:20 +0900

    test commit(compare with master branch)

--- app/src/main/java/com/nids/views/JoinActivity.java
+++ app/src/main/java/com/nids/views/JoinActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import com.nids.kind4u.testapp.R;
 
 public class JoinActivity extends AppCompatActivity {
 
+    EditText idText;
     Button btn_signup;
     Button btn_duplicate;
     @Override
