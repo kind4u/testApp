61183c459597b17b401c284a34230826616fa6e7
soobin519 <soobin0519@naver.com>
2020-04-20 16:25:53 +0900

    미세먼지 농도 상단바 알림

--- app/src/main/java/com/nids/views/OutsideFragment.java
+++ app/src/main/java/com/nids/views/OutsideFragment.java
@@ -1,9 +1,13 @@
 package com.nids.views;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -86,19 +90,66 @@ public class OutsideFragment extends Fragment {
             dustText.setText(outDoorData.getPM100() + "㎍/㎥");
             if (pm10 > 75.0) {
                 infoText.setText("매우나쁨");
+                createNotification4();
             } else if (pm10 > 35.0) {
                 infoText.setText("나쁨");
+                createNotification3();
             } else if (pm10 > 15.0) {
                 infoText.setText("보통");
+                createNotification2();
             } else {
                 infoText.setText("좋음");
+                createNotification1();
             }
             lat.setText(String.valueOf(map.get("lat")));
             lon.setText(String.valueOf(map.get("lon")));
         }
     }
 
-
+    public void createNotification1(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "default");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("미세먼지 농도 알림");
+        builder.setContentText("미세먼지 농도 : 좋음");
+        NotificationManager notificationManager= (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        notificationManager.notify(1, builder.build());
+    }
+    public void createNotification2(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "default");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("미세먼지 농도 알림");
+        builder.setContentText("미세먼지 농도 : 보통");
+        NotificationManager notificationManager= (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        notificationManager.notify(1, builder.build());
+    }
+    public void createNotification3(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "default");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("미세먼지 농도 알림");
+        builder.setContentText("미세먼지 농도 : 나쁨");
+        NotificationManager notificationManager= (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        notificationManager.notify(1, builder.build());
+    }
+    public void createNotification4(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "default");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("미세먼지 농도 알림");
+        builder.setContentText("미세먼지 농도 : 매우나쁨");
+        NotificationManager notificationManager= (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        notificationManager.notify(1, builder.build());
+    }
 
 //    private  void setData(VOOutdoor data){
 //        stationText.setText("실외 미세먼지 현황 ("+ map.get("lan") +"측정소)");
